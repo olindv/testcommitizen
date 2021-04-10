3628919abd7323929d72eb61ce1e94e922d11dac
dmitry olin <1988navras@gmail.com>
2021-04-10 22:38:31 +0300

    fdsf

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React. Test 3
+          Learn React. Test 5
         </a>
       </header>
     </div>
