f0b63a941d8ca0d32c70ccc977bf1bc955c34e46
dmitry olin <1988navras@gmail.com>
2021-04-12 12:32:00 +0300

    feat(PBM-333): sdfg
    zzz
    dfsfd
    
    META DATA:  aa zzz xx

--- config/git/commitizen.js
+++ config/git/commitizen.js
@@ -2,23 +2,13 @@
 
 module.exports = {
   types: [
-    {
-      value: "build",
-      name: "build:     Build the project or change external dependencies",
-    },
-    { value: "ci", name: "ci:        Настройка CI и работа со скриптами" },
+    { value: "build", name: "build:     Build the project or change external dependencies", },
+    { value: "ci", name: "ci:        Setting up CI and working with scripts" },
     { value: "docs", name: "docs:      Setting up CI and working with scripts" },
     { value: "feat", name: "feat:      Add new feature" },
     { value: "fix", name: "fix:       Bug fix" },
-    {
-      value: "perf",
-      name: "perf:      Changes to improve performance",
-    },
-    {
-      value: "refactor",
-      name:
-        "refactor:  Code changes without fixing bugs or adding new features",
-    },
+    { value: "perf", name: "perf:      Changes to improve performance" },
+    { value: "refactor", name: "refactor:      Code changes without fixing bugs or adding new features" },
     { value: "revert", name: "revert:    Revert to previous commits" },
     { value: "test", name: "test:      Add tests" },
   ],
@@ -27,11 +17,9 @@ module.exports = {
     type: "What changes are you making?",
     customScope: "Specify the scope:",
     subject: "Write a short description in the imperative:\n",
-    body:
-      'Write a detailed description (optional). Use "|" for a new line:\n',
+    body: 'Write a detailed description (optional). Use "|" for a new line:\n',
     breaking: "BREAKING CHANGES list (optional):\n",
-    footer:
-      "A place for meta data (tickets, links and the rest).\n",
+    footer: "A place for meta data (tickets, links and the rest).\n",
     confirmCommit: "Are you satisfied with the resulting commit?",
   },
 
@@ -39,7 +27,7 @@ module.exports = {
 
   allowBreakingChanges: false,
 
-  footerPrefix: "META DATA:",
+  footerPrefix: "META DATA: ",
 
   subjectLimit: 72,
 };
